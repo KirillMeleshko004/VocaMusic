4af241ded2f26414816ecfb08ede3235fed49a84
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-09-30 14:03:11 +0300

    Implemented footer

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,4 +8,7 @@ import { RouterLink } from '@angular/router';
   templateUrl: './logo.component.html',
   styleUrl: './logo.component.css',
 })
-export class LogoComponent {}
+export class LogoComponent {
+  @Input()
+  isDark = false;
+}
